c2717d27a537943d20c4c07e2310e6e2831ce7dd
Lena-PC\IvanyCH008 <chernets.ivan@gmail.com>
2017-08-18 10:38:17 +0300

    React-Redux from MonsterLessons. #1.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+/*import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
@@ -8,3 +8,25 @@ import App from './App';
 registerServiceWorker();
 
 ReactDOM.render(<App />, document.getElementById('root'));
+*/
+
+import {createStore} from 'redux';
+
+function playlist(state = [], action){
+    if(action.type === 'ADD_TRACK'){
+        return [
+            ...state,
+            action.payload
+        ];
+    }
+    return state;
+}
+
+const store = createStore(playlist);
+
+store.subscribe(()=> {
+    console.log('subscribe', store.getState());
+});
+
+store.dispatch({type: 'ADD_TRACK', payload: 'Smells like spirit'});
+store.dispatch({type: 'ADD_TRACK', payload: 'Enter Sandman'});
